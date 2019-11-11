7e5a46d93e76f35b0a02b8739d3af090c351684a
liv-ea <51425339+liv-ea@users.noreply.github.com>
2019-11-11 16:35:07 -0300

    Update listaencadeada.c

--- listaencadeada.c
+++ listaencadeada.c
@@ -1,1 +1,61 @@
+/* Bibliotecas */
 
+#include <stdio.h>
+#include <windows.h>
+
+/* Struct que armazena dados dos alunos */
+
+typedef struct aluno {
+
+  int matricula;
+
+  float nota1,
+        nota2,
+        nota3;
+
+} Aluno;
+
+typedef struct lista{
+
+  Aluno infor;
+
+  struct Lista* prox;
+
+} Lista;
+
+/* Protótipos de funções */
+
+ Lista* criarlista();
+
+int tamanholista();
+int inseriraluno();
+
+void liberarlista();
+
+/* Variáveis Globais */
+
+
+
+int main(){
+
+  system("color f0");
+
+  printf("ok\n");
+
+}
+
+/* struct Lista* criarlista(){
+
+} */
+
+int tamanholista(){
+
+}
+
+int inseriraluno(){
+
+}
+
+void liberarlista(){
+
+}
